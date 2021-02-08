137249d90731373e40363d065ccfedfe87961e06
sam.shi <32320811@qq.com>
2021-02-08 11:05:05 +0800

    1.0.4

--- public/index.php
+++ public/index.php
@@ -1,15 +1,8 @@
 <?php
-namespace rephp;
+use rephp\framework\app;
 //加载composer自动加载类
-
-
 require dirname(__DIR__) . '/vendor/autoload.php';
 //设置app运行目录
 $appPath = dirname(__DIR__).'app/';
 //运行框架
-use \NoahBuscher\Macaw\Macaw;
-Macaw::get('/', function() {
-    echo 'Hello world!';
-});
 (new app($appPath))->run();
-exit('ddd');
