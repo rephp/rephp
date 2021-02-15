e9182d9c0723eedb3bac8c12efdcbdfb9bb23ba7
sadxy <redphp@126.com>
2021-02-15 11:45:48 +0800

    修复错误

--- app/config/config.php
+++ app/config/config.php
@@ -1,1 +1,6 @@
 <?php
+return [
+    'time_zone' => 'PRC',
+
+];
+
--- public/index.php
+++ public/index.php
@@ -3,6 +3,6 @@ use rephp\framework\app;
 //加载composer自动加载类
 require dirname(__DIR__) . '/vendor/autoload.php';
 //设置app运行目录
-$appPath = dirname(__DIR__).'app/';
+$appPath = dirname(__DIR__).'/app/';
 //运行
 (new app())->run($appPath);
