237d95a50eba5b01adc5e82040e7e0ef401760c3
sam.shi <32320811@qq.com>
2021-02-19 18:04:49 +0800

    test

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 use rephp\framework\app;
 //加载composer自动加载类
 require dirname(__DIR__) . '/vendor/autoload.php';
+
 //设置app运行目录
 $appPath = dirname(__DIR__).'/app/';
 //运行
--- route/route.php
+++ route/route.php
@@ -4,4 +4,9 @@ use \NoahBuscher\Macaw\Macaw;
 Macaw::get('', 'app\\modules\\index\\controller\\testController@testAction');
 
 Macaw::get('test/(:num)', 'app\\modules\\index\\controller\\testController@test2Action');
+
+Macaw::$error_callback = function() {
+    throw new Exception("路由无匹配项 404 Not Found");
+};
+
 Macaw::dispatch();
