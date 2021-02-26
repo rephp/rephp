104671def698b143d447526d364178ee7b610c88
sadxy <redphp@126.com>
2021-02-26 23:19:54 +0800

    update

--- route/route.php
+++ route/route.php
@@ -2,5 +2,5 @@
 use rephp\framework\component\route\route;
 
 
-route::get('index/test(:all)', 'app\\modules\\index\\controller\\testController@testAction');
-route::get('index/test/test2(:all)', 'app\\modules\\index\\controller\\testController@testAction');
+//route::get('index/test/test3(:all)', 'app\\modules\\index\\controller\\testController@test2Action');
+//route::get('index/test(:all)', 'app\\modules\\index\\controller\\testController@testAction');
