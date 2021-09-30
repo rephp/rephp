b0b0db8f54d3f85f7cbc50a46e176d61f9617ee3
sam.shi <32320811@qq.com>
2021-09-30 18:42:22 +0800

    test

--- route/2222.php
+++
@@ -1,6 +0,0 @@
-<?php
-use rephp\component\route\route;
-
-
-route::get('index/test/test3(:all)', 'app\\modules\\index\\controller\\testController@test2Action');
-//route::get('index/test2(:all)', 'app\\modules\\index\\controller\\testController@testAction');
