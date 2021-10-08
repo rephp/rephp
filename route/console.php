4829c1f56d30f0dc8fbcd2b939e5dc97c1854a36
sam.shi <32320811@qq.com>
2021-10-08 19:00:28 +0800

    test

--- route/console.php
+++ route/console.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'test3' => ['method'=>'get', 'class'=>'app\\console\\member\\test@test2'],
-    'index/test/test4' => ['method'=>'get'],
-    'index/test/test2' => ['method'=>'get', 'class'=> 'app\\modules\\index\\controller\\testController@testAction'],
+    'test3' => ['method'=>'get', 'class'=>'app\\console\\member\\test@test2', 'desc'=>'tttt test'],
+    'index/test/test4' => ['method'=>'get', 'desc'=>'tttt test'],
+    'index/test/test2' => ['method'=>'get', 'class'=> 'app\\modules\\index\\controller\\testController@testAction', 'desc'=>'tttt test'],
 ];
