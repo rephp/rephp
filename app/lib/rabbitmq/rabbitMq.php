3d0436c7b226f8cde54f0e9f3a2d4def3c0a4d64
shixingyu <32320811@qq.com>
2023-05-19 17:01:18 +0800

    bug

--- app/lib/rabbitmq/rabbitMq.php
+++ app/lib/rabbitmq/rabbitMq.php
@@ -7,29 +7,37 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
 /**
- * Class rabbitMq
+ * Class RabbitMq
  * @package common\utils\sync
  */
-class rabbitMq
+class RabbitMq
 {
+    /**
+     * 引入日志操作方法
+     */
+    use logTrait;
+
     /**
      * 推送数据
      * @param string $queueName 队列名字
      * @param mixed  $message   消息内容
      * @return array
      */
-    public function send($queueName, $message = '')
+    public static function send($queueName, $message = '', $config = [])
     {
-        $config   = config('rabbitmq');
-        $exchange = $queueName . '_exchange';
-        $routeKey = $queueName . '_route';
+        if (empty($queueName)) {
+            return ['code' => 445, 'msg' => '队列名不能为空'];
+        }
+        empty($config) && $config   = config('rabbitmq');
         if (empty($config)) {
             return ['code' => 443, 'msg' => '队列配置错误，请检查并重试'];
         }
+        $exchange = $queueName;
+        $routeKey = $queueName;
         //统一数据格式
         is_array($message) && $message = json_encode($message);
         //连接中间件
-        $ququeClient = new rabbitMqLib($config, $exchange, $queueName, $routeKey);
+        $ququeClient = new RabbitMQCommand($config, $exchange, $queueName, $routeKey);
         //开始发送数据
         $res = $ququeClient->send($message);
 
@@ -37,23 +45,28 @@ class rabbitMq
     }
 
     /**
-     * 请求数据，并回调方法执行代码
-     * @param string $queueName          队列名
-     * @param string $callbackMethodName 回调方法名
-     * @param object $callbackObject     回调对象
-     * @return array
+     * 推送领券卡券详情到分站
+     * @param string $queueName      队列名
+     * @param string $dealMethodName 消费类的处理方法名
+     * @return array|bool
+     * @example php yii url
      */
-    public function startDealMsg($queueName, $callbackMethodName, $callbackObject)
+    public static function get($queueName, $dealMethodName, $config = [])
     {
-        $config   = config('rabbitmq');
-        $exchange = $queueName . '_exchange';
-        $routeKey = $queueName . '_route';
-        if (empty($config)) {
+        if (empty($queueName) || empty($classObj) || empty($dealMethodName)) {
+            return false;
+        }
+        empty($config) && $config   = config('rabbitmq');
+        $exchange = $queueName;
+        $routeKey = $queueName;
+        if (empty($config) || empty($exchange) || empty($routeKey)) {
             return ['code' => 443, 'msg' => '队列配置错误，请检查并重试'];
         }
         //连接中间件
-        $ququeClient = new rabbitMqLib($config, $exchange, $queueName, $routeKey);
-        $result      = $ququeClient->run([$callbackObject, $callbackMethodName], false);
+        $ququeClient    = new RabbitMQCommand($config, $exchange, $queueName, $routeKey);
+        $calldClassName = get_called_class();
+        $classObj       = new $calldClassName();
+        $result         = $ququeClient->run([$classObj, $dealMethodName], false);
 
         die(json_encode($result));
     }
